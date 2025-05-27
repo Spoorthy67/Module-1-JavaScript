0bb6c2516e02daf2dd4d32af2b67dae4e16fcf29
Spoorthy67 <mamillapallyspoorthy@gmail.com>
2025-05-27 09:32:24 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Log welcome message to console
+console.log("Welcome to the Community Portal");
+
+// Show alert when the page is fully loaded
+window.onload = function () {
+  alert("The Community Portal page is fully loaded!");
+};
